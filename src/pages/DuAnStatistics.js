dbb3a74573c9d674f558916783c4ca875a1a175f
ninhpoal <132538119+ninhpoal@users.noreply.github.com>
2025-04-09 15:26:29 +0700

    Update DuAnStatistics.js

--- src/pages/DuAnStatistics.js
+++ src/pages/DuAnStatistics.js
@@ -58,7 +58,7 @@ const DuAnStatistics = () => {
         try {
           setLoading(true);
           const response = await authUtils.apiRequest('CHI', 'Find', {Properties: {
-            Selector: `Filter(CHI, and([TRẠNG THÁI] = "Đã giải ngân",[TKCHI]="BIDV Nam Phạm"))`
+            Selector: `Filter(CHI, [TRẠNG THÁI] = "Đã giải ngân")`
         }});
           
           // Transform dữ liệu chi
